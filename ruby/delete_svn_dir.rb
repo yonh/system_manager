8a6d88ed84137d358f740a0c799d9d0fa7f20d20
yonh <azssjli@163.com>
2016-03-09 18:29:31 +0800

    add delete svn folder script

--- ruby/delete_svn_dir.rb
+++ ruby/delete_svn_dir.rb
@@ -1,5 +1,23 @@
 #!/usr/bin/ruby
 
+require 'find'
+
+#default current dir
+
+dir = "."
+
+count = 0
+
+Find.find(dir) do |path|
+  if File.directory?(path) and File.basename(path) == '.svn'
+        FileUtils.rm_r path
+        count = +1
+  end
+end
+
+puts "info: success remove #{count} dir"
+
+exit
 File.open("a.svn", "r") do |f|
 	while s = f.gets do
 		s = s.rstrip
