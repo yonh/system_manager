03bc96a680edfa77b991da740f1a6a08c62174e3
yonh <azssjli@163.com>
2016-11-02 10:51:43 +0800

    修改变量命名

--- shell/inotify_bak.sh
+++ shell/inotify_bak.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 src=/www
-dist=/www/
+dest=/www/
 toip=11.11.11.11
 inotifywait -mrq --timefmt '%d/%m/%y %H:%M' --format '%T %w%f%e' -e close_write,delete,create,attrib $src |  while read file
 do
-	rsync -raz --delete  --exclude-from ignore_file  $src root@$toip:$dist
+	rsync -raz --delete  --exclude-from ignore_file  $src root@$toip:$dest
 done
