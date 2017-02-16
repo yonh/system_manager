afec2d6a5d5362c9899b29c429597b2c98d5e40a
yonh <azssjli@163.com>
2017-02-16 10:10:13 +0800

    Update docker-install.sh

--- shell/docker-install.sh
+++ shell/docker-install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 apt-get update
 #apt-get upgrade -y
-apt-get install -y apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >  /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get purge lxc-docker
-sudo apt-get install -y docker-engine
+apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+
+apt-get install -y docker-engine
